e7fb3f58660cbf8c7df69b0994553fe0485f96ec
Mauricio <info@webspymessalamanca.com>
2018-09-10 21:48:31 +0200

    no message

---
+++ src/app/first.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first-component',
+  template: '<h2>First Component Header</h2><h3>First Component Sub Header</h3>'
+})
+
+export class FirstComponent {}
---
+++ src/app/second.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'second-component',
+  template: '<p>This is the second component parragraph</p><p>This is the second component parragraph</p><p>This is the second component parragraph</p>'
+
+})
+
+export class SecondComponent { }
